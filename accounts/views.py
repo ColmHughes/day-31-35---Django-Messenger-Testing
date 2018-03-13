0b248f29e74f25d7bfe0b9ea8a0a92acf1788248
Colm Hughes <cjhugo1991@hotmail.com>
2018-03-13 10:32:37 +0000

    failed test

--- accounts/views.py
+++ accounts/views.py
@@ -34,7 +34,7 @@ def login(request):
     
     return render(request, 'accounts/login.html', { 'form': form })
 
-@login_required()
+# @login_required()
 def profile(request):
     return render(request, 'accounts/profile.html')
     
